6eda640a9e69506bdad62cb861cc64c27b1bf889
Robert Bryan <bryarobert@gmail.com>
2017-06-06 15:12:34 -0400

    longest word and stuff

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 function wordCount($scope) {
     $scope.dummy = 'stupid';
-    $scope.inputText = "";
-    $scope.count;
-	$scope.$watch("words", textCounter);
 
-	function textCounter(input) {
-		if (input) {
-			let arr = input.split(' ');
-			$scope.count = arr.length;
-		}
+	$scope.textCounter = function () {
+		 if ($scope.words){
+         return $scope.words.split(" ").length;
+       }
+			
 	};
+
+   $scope.longest = function (){ 
+      if ($scope.words){
+         let arr = $scope.words.split(" ");
+         arr.sort((curr,prev) => prev.length - curr.length);
+         return arr.filter( a => a === arr[0]).join(",");
+      } 
+   };
 }
  
 
