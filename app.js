3fb64a12e48cd94471a408930ba7ab427802a935
Sean Montgomery <seancmont@gmail.com>
2017-06-06 14:32:08 -0400

    made it work

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 function wordCount($scope) {
     $scope.dummy = 'stupid';
     $scope.inputText = "";
+    $scope.count;
+	$scope.$watch("words", textCounter);
 
-    
+	function textCounter(input) {
+		if (input) {
+			let arr = input.split(' ');
+			$scope.count = arr.length;
+		}
+	};
 }
+ 
 
 
 
